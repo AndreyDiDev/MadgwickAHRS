a402f3785929d181839f659a20ee6cdc1a6ea6aa
Andrey D <andinfbi@gmail.com>
2024-06-04 12:14:51 -0600

    Update advanced_example.py

--- Python/advanced_example.py
+++ Python/advanced_example.py
@@ -12,14 +12,18 @@ import matplotlib.pyplot as pyplot
 import numpy
 
 # Import sensor data
-data = numpy.genfromtxt("C:/Users/Andrey/Documents/Fusion-main/Fusion-main/Python/sensor_data.csv", delimiter=",", skip_header=1)
+data = numpy.genfromtxt("C:/Users/Andrey/Documents/Fusion-main_new/Fusion-main/Fusion/Imu 1.csv", delimiter=",", skip_header=1)
 
-sample_rate = 100  # 100 Hz
+sample_rate = 3  # 100 Hz
 
-timestamp = data[:, 0]
-gyroscope = data[:, 1:4]
-accelerometer = data[:, 4:7]
-magnetometer = data[:, 7:10]
+timestamp = data[:, 9]
+
+for i in range(len(timestamp)):
+    timestamp[i] = i * 1/3
+    
+gyroscope = data[:, 3:6]
+accelerometer = data[:, 0:3]
+magnetometer = data[:, 6:9]
 
 print(numpy.shape(data))
 
@@ -30,11 +34,11 @@ ahrs = imufusion.Ahrs()
 
 
 ahrs.settings = imufusion.Settings(imufusion.CONVENTION_NED,
-                                   0.5,  # gain
-                                   2000,  # gyroscope range
-                                   10,  # acceleration rejection
-                                   10,  # magnetic rejection
-                                   5 * sample_rate)  # recovery trigger period = 5 seconds
+                                    0.5,  # gain
+                                    245,  # gyroscope range
+                                    10,  # acceleration rejection
+                                    10,  # magnetic rejection
+                                    5 * sample_rate)  # recovery trigger period = 5 seconds
 
 # Process sensor data
 print(timestamp[-2] - timestamp[-1])
@@ -45,6 +49,8 @@ euler = numpy.empty((len(timestamp), 3))
 internal_states = numpy.empty((len(timestamp), 6))
 flags = numpy.empty((len(timestamp), 4))
 
+filteredEarthAcceleration = numpy.empty((len(timestamp), 1))
+filteredLinearAcceleration = numpy.empty((len(timestamp), 1))
 
 print(delta_time[:])
 # initialize linear acceleration
@@ -56,11 +62,14 @@ for index in range(len(timestamp)):
     
     # print(ahrs.earth_acceleration)
     
-    gyroscope[index] = offset.update(gyroscope[index])
+    gyroscope[index] = offset.update(gyroscope[index]/1000)
 
-    ahrs.update(gyroscope[index], accelerometer[index], magnetometer[index], delta_time[index])
+    ahrs.update_no_magnetometer(gyroscope[index]/1000, accelerometer[index]/1000, 1/3)
 
     euler[index] = ahrs.quaternion.to_euler()
+    
+    filteredEarthAcceleration[index] = ahrs.earth_acceleration[2]
+    filteredLinearAcceleration[index] = ahrs.linear_acceleration[2]
 
     ahrs_internal_states = ahrs.internal_states
     internal_states[index] = numpy.array([ahrs_internal_states.acceleration_error,
@@ -140,3 +149,105 @@ axes[9].legend()
 plot_bool(axes[10], timestamp, flags[:, 3], "Magnetic recovery")
 
 pyplot.show(block="no_block" not in sys.argv)  # don't block when script run by CI
+
+#########################################################################
+
+# Create subplots
+fig, (ax1, ax2) = pyplot.subplots(2, 1, figsize=(8, 6))
+
+# Plot gyro data
+ax1.plot(timestamp, gyroscope[:, 0], label='GyroX', color='red')
+ax1.plot(timestamp, gyroscope[:, 1], label='GyroY', color='green')
+ax1.plot(timestamp, gyroscope[:, 2], label='GyroZ', color='blue')
+ax1.set_title('Gyro Data')
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Angular Velocity')
+ax1.grid(True)
+ax1.legend()
+
+# Plot accelerometer data
+ax2.plot(timestamp, accelerometer[:, 0], label='AccelerometerX', color='orange')
+ax2.plot(timestamp, accelerometer[:, 1], label='AccelerometerY', color='purple')
+ax2.plot(timestamp, accelerometer[:, 2], label='AccelerometerZ', color='brown')
+ax2.set_title('Accelerometer Data')
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel('Acceleration')
+ax2.grid(True)
+ax2.legend()
+
+pyplot.tight_layout()
+pyplot.show()
+
+#########################################################################
+# import altimeter data
+data1 = numpy.genfromtxt("C:/Users/Andrey/Documents/EverestRepo/Apogee-Detection-Everest/MadgwickLibrary/Altimeter.csv", delimiter=",", skip_header=1)
+
+
+time = data1[:, 0]
+acceleration = data1[:, 1]
+
+# Convert to m/s²
+conversion_factor = 0.3048  # 1 ft/s² = 0.3048 m/s²
+acceleration_m = acceleration
+
+# Create subplots
+fig, (ax1, ax2) = pyplot.subplots(2, 1, figsize=(8, 6), sharex=True)
+
+# Plot data
+ax1.plot(time, acceleration_m, color='red')
+ax1.set_title('Alt Accel')
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('Acceleration')
+ax1.grid(True)
+ax1.legend()
+
+# Plot accelerometer data
+ax2.plot(timestamp, (accelerometer[:, 0]/1000) * (-9.81), label = "Raw", color='orange')
+ax2.plot(timestamp, filteredEarthAcceleration * (-9.81), label = "Filtered", color='purple')
+# ax2.plot(timestamp, filteredLinearAcceleration, label = "Linear", color='green')
+ax2.set_title('Accel IMU')
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel('Acceleration')
+ax2.grid(True)
+ax2.legend()
+
+pyplot.tight_layout()
+pyplot.show()
+
+#########################################################################
+dataBaro = numpy.genfromtxt("C:/Users/Andrey/Documents/EverestRepo/Apogee-Detection-Everest/MadgwickLibrary/baro 3.csv", delimiter=",", skip_header=1)
+# graph altitude 
+height = data1[:, 3]
+
+pressure = dataBaro[:, 0] # in Pa
+
+seaLevelPressure = 1013.25 * 1000; # sea level pressure in hPa
+altitudeBaro = 44330.0 * (1.0 - pow(pressure / seaLevelPressure, 0.190284))
+
+# Convert to m
+# altitudeBaro = 
+
+# height_m = height * 0.3048
+
+# Create subplots
+fig, (ax1, ax2) = pyplot.subplots(2, 1, figsize=(8, 6), sharex=True)
+
+
+ax1.plot(time, height, label = "Altimeter", color='red')
+ax1.set_title('Alt height')
+ax1.set_xlabel('Time (s)')
+ax1.set_ylabel('meters')
+ax1.grid(True)
+ax1.legend()
+
+timeS = timestamp[:543]
+
+ax2.plot(timeS, altitudeBaro * 0.3048, label = 'Baro', color='blue')
+ax2.set_title('Baro altitude')
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel('meters')
+ax2.grid(True)
+ax2.legend()
+
+pyplot.tight_layout()
+pyplot.show()
